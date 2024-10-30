c2f76af83995c9986fa560b2c3a556ebbb457e33
Garrett Cutler <cutlerwater2@live.com>
2024-10-30 17:20:14 -0400

    Settings Commit

--- dentist/settings.py
+++ dentist/settings.py
@@ -1,4 +1,7 @@
 import os
+import django_heroku
+import dj_database_url
+from decouple import config 
 
 from pathlib import Path
 
@@ -40,6 +43,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'dentist.urls'
@@ -114,7 +118,17 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = 'outlook.office365.com'
+EMAIL_PORT = 993
+EMAIL_HOST_USER = 'cutlerwater2@live.com'
+EMAIL_HOST_PASSWORD = 'Lastsupper44%'
+EMAIL_USE_TLS = True
+
+django_heroku.settings(locals())
